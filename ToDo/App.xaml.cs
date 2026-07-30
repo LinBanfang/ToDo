3a6fc4f0167dd6286469b73d658713abea8e771b
LinBanfang <wubin921@gmail.com>
2026-07-30 09:59:43 +0800

    feat: add blue app icon

--- ToDo/App.xaml.cs
+++ ToDo/App.xaml.cs
@@ -17,6 +17,12 @@ public partial class App : Application
         ViewModel = new MainViewModel(Database);
 
         var mainWindow = new MainWindow { DataContext = ViewModel };
+        try
+        {
+            mainWindow.Icon = new System.Windows.Media.Imaging.BitmapImage(
+                new Uri("pack://application:,,,/Resources/app.ico"));
+        }
+        catch { /* use default icon */ }
         mainWindow.Show();
     }
 
